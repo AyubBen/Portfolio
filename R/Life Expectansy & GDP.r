732c361f21e3e01fc96ff6f612d65aaf2c6c1d4c
Ayub Benlahcene <91146785+AyubBen@users.noreply.github.com>
2022-10-17 13:57:03 +0100

    Update Life Expectansy & GDP.r

--- R/Life Expectansy & GDP.r
+++ R/Life Expectansy & GDP.r
@@ -1,3 +1,62 @@
+#Introduction
+"This project will use Excel, R and SPSS.
+
+The dataset used can be found in Kaggle:
+https://www.kaggle.com/tomasdrietomsky/life-expectancy-gdp-per-capita-food-security
+
+The dataset chosen for this case study is GDP & Life Expectancy. The data was retrieved from an open source dataset found in Kaggle, 
+and it is made-up with two main excel sheets. The first is about life expectancy recorded over a yearly course between 2000 – 2015. 
+A total of 264 countries will be available for analysis use, providing 2905 total rows. The second will be about the GDP of each country between 1960-2019. 
+Each country’s life expectancy is different than another due to health development, economic stability and many more factors."
+
+
+
+#Business Case Scenario (I created my own case):
+"The business case of this study is to investigate the physical health of the population in developing and developed countries over the years, 
+and whether GDP played a role in equation.
+Attributes such as GDP, life expectancy, adult mortality, infant deaths are all promising attributes to analyze and find interesting predictions. 
+Two countries have been chosen to narrow the focus of the study and make it more impactful and specific. The countries are first divided into two sections:"
+
+
+
+#Business Questions:
+"•	What is the future direction of GDP in the stated countries?
+•	What is the future direction for life expectancy, and some of the other variable in the stated countries?
+•	Which attributes are affected by the high/low GDP of the stated countries?"
+
+
+
+#The way to a successful forecast goes through various steps:
+"• Cleaning the data.
+• Analyzing, visualizing & understanding the data.
+• Perform Forecasting and predictive methods.
+• Provide Recommendations based on results."
+
+#I. Data and Preprocessing (cleaning, missing values, recoding)
+"Cleaning and checking for missing data were done in Excel. 
+The columns of years between 1960-1999 and 2016-2019 will also be removed, due to having only the years between 2000-2015 in the Life Expectancy sheet. 
+Due to the lack of documentation, the following attributes were removed from both sheets: 
+•	Schooling
+•	Percentage Expenditure
+•	Total expenditure
+•	Income composition of resources
+•	Hepatitis
+•	Country Code
+•	Indicator Name
+•	Indicator Code"
+
+"columns with missing data in rows:
+- Schooling 163 ( deleted column)
+- Income composition of resources 167 ( deleted column )
+- Total expenditure 226 ( deleted column ) 
+- Population 652 ( deleted column )
+- Hepatitis 553 ( deleted column)
+- GDP 448 ( Added data for years that will be analyzed )
+- Alcohol 194 rows ( manually added them )
+- Polio 19 ( manually added them )
+- Diphtheria 19 ( manually added them ) 
+- thinness 1-19 years 34 ( manually added them ) 
+- thinness 5-9 years 34 ( manually added them )" 
 
 #Libraries that will be used for this project
 library(readxl)
@@ -9,9 +68,6 @@ library(feasts)
 
 
 
-#Importing the data into R
-Life_Expectancy_Data <- read.csv("Project.csv")
-
 #Information about the dataset
 summary(Life_Expectancy_Data)
 summary(GDP)
@@ -19,26 +75,36 @@ view(Life_Expectancy_Data)
 view(GDP)
 
 
+# ADD SCREENSHOTS!!
+
+
+
+
+
 #Check if there are any null values
 colSums(is.na(Life_Expectancy_Data))
 colSums(is.na(GDP))
+
+# ADD SCREENSHOTS!!
+
+
 
 #Creating the variable Time to only date 
 system.time(Life_Expectancy_Data$date <- as.Date(Life_Expectancy_Data$date))
 
 
 
+#Due to a duplication in some of the values, the first function to use is Validate, 
+#and the focus is on the validate function which validates that the output has all the necessary inputs before rendering. 
 
 #validate equals to false to not check duplicated values and key = null if we dont have any keys
 Life_Expectancy_Data %>% as_tsibble(key = Country,index = date, validate = FALSE)
 
 
-
+#Next, the tsibble function is used to convert the tibble of the two countries into a time series
 #make a tsibble
 Life_Expectancy_Data_ts <- Life_Expectancy_Data %>%  mutate(Annual = date) %>%
   as_tsibble(index = Annual, key = c(Country),validate = FALSE)
-
-
 
 
 #make tsibbles by group by countries (China, Germany)
@@ -52,7 +118,7 @@ Germany <- Life_Expectancy_Data_ts  %>% filter(Life_Expectancy_Data_ts$Country =
   as_tsibble(index = Annual, key = NULL)
 
 
-
+#Produce early visualization for the China and Germany’s different attributes
 
 # Early visualizations to learn more about the data.
 China %>% autoplot(Life.Expectancy)
@@ -70,8 +136,6 @@ China %>% autoplot(Thinness.1.4.Years.Old)
 China %>% autoplot(Thinness.5.9.Years.Old)
 
 
-
-
 Germany %>% autoplot(Life.Expectancy)
 Germany %>% autoplot(Adult.Mortality)
 Germany %>% autoplot(Infant.Deaths)
@@ -85,6 +149,10 @@ Germany %>% autoplot(Diphtheria)
 Germany %>% autoplot(GDP)
 Germany %>% autoplot(Thinness.1.4.Years.Old)
 Germany %>% autoplot(Thinness.5.9.Years.Old)
+
+
+
+#ADD SCREENSHOTSSS !!!
 
 
 
@@ -107,6 +175,8 @@ Germany %>% autoplot(GDP,color='gray')+
 components(Germany_dcmp) %>% autoplot()+ xlab("Year")
 
 
+
+#III. Making Predictions¶
 
 #Simple Forecast Methods
 Countries_fit <- Life_Expectancy_Data_ts %>% 
